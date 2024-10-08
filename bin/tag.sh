8b16b370759a837215c99a8abf908bd246117b43
Jon Goodger <jonno.is@gmail.com>
2024-10-08 15:23:29 +0100

    Fix tagging as $MAIN_TAG instead of "latest"

--- bin/tag.sh
+++ bin/tag.sh
@@ -4,5 +4,5 @@
 
 echo Tagging image $IMAGE_NAME as \"$MAIN_TAG\" and \"$SPECIFIC_TAG\"...
 
-docker tag $IMAGE_NAME:latest $IMAGE_NAME:latest
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:$MAIN_TAG
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:$SPECIFIC_TAG
